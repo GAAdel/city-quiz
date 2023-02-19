f5bd36c7b99f966e70cc5f5d5aba203c049f25d7
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:22:06 +0300

    [WIP] Create main-card components

---
+++ src/components/main-card/CardBody.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-body text-sm leading-normal text-zinc-700 p-6">
+    <div class="card-body__text mb-6">Цель: Назвать как можно больше реальных городов.</div>
+    <ul class="card-body__list mb-6 list-disc">
+      <li class="card-body__item ml-6">
+        Запрещается повторение городов.
+      </li>
+      <li class="card-body__item ml-6">
+        Названий городов на твердый “ъ” и мягкий “ъ” знак нет. Из-за этого бы пропускаем эту букву и игрок должен назвать город на букву стоящую перед ъ или ь знаком.
+      </li>
+      <li class="card-body__item ml-6">
+        Каждому игроку дается 2 минуты на размышления, если спустя это время игрок не вводит слово он считается проигравшим
+      </li>
+    </ul>
+    <button class="block mx-auto bg-violet-600 text-white rounded px-4 py-2" type="button" @click="handleClick">Начать игру</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const handleClick = () => {
+  router.push('/play');
+}
+
+</script>
+
+<style scoped></style>
---
+++ src/components/main-card/CardHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="card-header text-center border-b-[3px] border-zinc-100 px-6 py-[17px]">
+    <h1 class="card-header__title text-base text-black">Игра в города на время</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
