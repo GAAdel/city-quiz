7197c11be9c3553614f6d141f89fff96272612ea
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:24:28 +0300

    Create HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,15 @@
 <template>
   <main>
+    <AppCard>
+      <CardHeader />
+      <CardBody />
+    </AppCard>
   </main>
 </template>
 
 <script setup lang="ts">
+import AppCard from '@/components/AppCard.vue';
+import CardHeader from '@/components/main-card/CardHeader.vue'
+import CardBody from '@/components/main-card/CardBody.vue'
 
 </script>
