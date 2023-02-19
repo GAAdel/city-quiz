f4b5781947d22b49518d57deaccd1a835cd759f0
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:25:47 +0300

    [WIP] Create FinalView

---
+++ src/views/FinalView.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <AppCard>
+      <CardBody />
+    </AppCard>
+  </main>
+</template>
+
+<script setup lang="ts">
+import AppCard from '@/components/AppCard.vue';
+import CardBody from '@/components/final-card/CardBody.vue';
+
+</script>
