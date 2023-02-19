7967475296a63bfa837d75ff8f4a57b44f2d7436
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:22:55 +0300

    [WIP] Create final-card component

---
+++ src/components/final-card/CardBody.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card-body text-xl leading-normal text-center px-20 py-10">
+    <div 
+      class="card-body__text leading-normal mb-8"
+      v-if="route.params.winner === 'computer'"
+    >К сожалению твое время вышло! <br> Твой противник победил!</div>
+
+     <div 
+      class="card-body__text leading-normal mb-8"
+      v-else
+    >Поздравляем тебя с победой! <br> Твой противник не вспомнил нужный город!</div>
+    
+    <div 
+      class="card-body__text text-3xl leading-normal mb-8"
+      :class="route.params.winner === 'computer' ? 'text-red-600' : 'text-green-600'"
+    >00:00</div>
+
+    <div class="card-body__text leading-normal mb-8">Всего было перечислено городов: {{ route.params.amount }} <br> Очень не плохой результат!</div>
+
+    <div class="card-body__text leading-normal mb-8">Последний город названный победителем <br> <span class="card-body__text text-2xl">{{ route.params.lastCity }}</span></div>
+
+    <button 
+      class="block mx-auto bg-violet-600 text-base text-white rounded px-4 py-2"
+      type="button"
+      @click="handleClick"
+    >Начать новую игру</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+
+const handleClick = () => {
+  router.push('/play');
+}
+
+</script>
+
+<style scoped></style>
