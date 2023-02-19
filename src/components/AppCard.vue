bdd3c6705ecf6dbd6828d3a536b785ecee8b4353
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:23:39 +0300

    [WIP] Create Card component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="
+    card w-[576px]
+    absolute
+    top-1/2
+    left-1/2
+    -translate-x-2/4
+    -translate-y-2/4
+    flex flex-col 
+    bg-white
+    rounded-2xl">
+
+    <slot />
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>  
