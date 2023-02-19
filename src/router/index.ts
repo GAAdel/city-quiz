7fd2e19217002a2d532025403d733ebacfd8e86d
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:19:45 +0300

    Add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,14 +9,16 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+    {
+      path: '/play',
+      name: 'play',
+      component: () => import('../views/PlayView.vue')
+    },
+    {
+      path: '/final/:winner/:lastCity/:amount',
+      name: 'final',
+      component: () => import('../views/FinalView.vue')
+    }
   ]
 })
 
