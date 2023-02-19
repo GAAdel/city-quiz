94f836439e3ddeb98d3cafc90ed5c91d4e493b2f
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:20:37 +0300

    Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "send-answer": "url('src/assets/img/send-answer.svg')",
+      },
+    },
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    fontFamily: {
+      sans: ["Helvetica Neue", "sans-serif"],
     },
   },
   plugins: [require("@tailwindcss/typography")],
