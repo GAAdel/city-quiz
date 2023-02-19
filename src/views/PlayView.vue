bdb4e5cedad29c89d55076f4f7720d962f24490f
CIKADA <adel.garipovazn@mail.ru>
2023-02-20 02:25:37 +0300

    [WIP] Create PlayView

---
+++ src/views/PlayView.vue
@@ -0,0 +1,86 @@
+<template>
+  <main>
+    <AppCard>
+      <CardHeader :timer="timer" />
+      <CardBody 
+        :currentTurn="currentTurn"
+        @changeCurrentTurn="changeCurrentTurn"
+      />
+    </AppCard>
+  </main>
+</template>
+
+<script setup lang="ts">
+import AppCard from '@/components/AppCard.vue';
+import CardHeader from '@/components/play-card/CardHeader.vue'
+import CardBody from '@/components/play-card/CardBody.vue'
+
+import { ref, onBeforeUnmount, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const isQuizFinished = ref(false);
+
+let deadline = new Date().getTime() + 120000;
+const timer = ref();
+// const timerPercentage = ref();
+
+const updateTimer = () => {
+  const date = new Date().getTime();
+  const timeRemaining = (deadline - date) / 1000;
+
+  const minutes = Math.floor((timeRemaining / 60) % 60);
+  const seconds = Math.floor(timeRemaining % 60);
+
+  const fMinutes = minutes < 10 ? '0' + minutes : minutes;
+  const fSeconds = seconds < 10 ? '0' + seconds : seconds;
+
+  if (timeRemaining <= 1) {
+    clearInterval(currentTimer);
+    isQuizFinished.value = true;
+  }
+
+  timer.value = `${fMinutes}:${fSeconds}`;
+  // timerPercentage.value = 100 * timeRemaining / 120000;
+}
+
+let currentTimer;
+
+const startTimer = () => {
+  currentTimer = setInterval(updateTimer, 500);
+}
+
+onBeforeUnmount(() => {
+  clearInterval(currentTimer);
+});
+
+(() => {
+  startTimer();
+})();
+
+const currentTurn = ref('user' as 'user' | 'computer');
+
+const lastCity = ref(' '); // ToDo
+const amount = ref(0); // ToDo
+
+const changeCurrentTurn = (lastAnswer: string, amountAnswers: number) => {
+  lastCity.value = lastAnswer;
+  amount.value = amountAnswers;
+  deadline = new Date().getTime() + 120000;
+  clearInterval(currentTimer);
+  startTimer();
+  if (currentTurn.value === 'user') {
+    currentTurn.value = 'computer';
+  } else {
+    currentTurn.value = 'user';
+  }
+}
+
+watch(isQuizFinished, (newNumber, oldNumber) => {
+  if (newNumber === true) {
+    router.push({ name: 'final', params: { winner: `${currentTurn.value}`, lastCity: lastCity.value, amount: amount.value } })
+  }
+});
+
+</script>
